d7c6e1054e743df34855b181004d0826ebc72cd8
断虹残雪 <chaofree3088@sina.com>
2018-01-22 22:51:44 +0800

    修改目录

--- Code/js/3_sql/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-	database: 'js_test',
-	username: 'root',
-	password: 'renford',
-	host: 'localhost',
-	part: 3306
-};
-
-module.exports = config;
--- Code/js/3_sql/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('koa-router')();
-
-const controllers = require('../controllers');
-
-router.get('/login', controllers.login);
-
-router.get('/user', controllers.user);
-
-module.exports = router;
